a5ce76161463600390b159a5c5f4df087f3a16d6
Keisuke Ota <arvelt.s@gmail.com>
2015-06-15 00:40:41 +0900

    add flaskapp

--- run.sh
+++ run.sh
@@ -8,8 +8,13 @@ if [ ${USERNAME} == "nothing" ]; then
     echo "Use vagrant user"
 fi
 
+vars="{
+    \"APP_USER_NAME\":\"$USERNAME\",
+    \"APP_REPOSITORY\":\"https://github.com/arvelt/hello-flask.git\"
+}"
 DIR=$(cd $(dirname $0); pwd)
 
 ansible-galaxy install gdamjan.uwsgi -p ${DIR}
 ansible-galaxy install jdauphant.nginx -p ${DIR}
-ansible-playbook -i hosts -u ${USERNAME} site.yml
+ansible-galaxy install vitalk.flaskapp -p ${DIR}
+ansible-playbook -i hosts -u ${USERNAME} --extra-vars="${vars}" site.yml
