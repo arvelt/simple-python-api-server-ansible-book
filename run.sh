4f7d8c44d959e371de0dd9695d48cfe7e8fed0d6
Keisuke Ota <arvelt.s@gmail.com>
2015-06-14 16:44:45 +0900

    nginx

---
+++ run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo -n "username:[default:vagrant]"
+read USERNAME
+if [ ! ${USERNAME} ]; then USERNAME="nothing"; fi
+if [ ${USERNAME} == "nothing" ]; then
+    USERNAME="vagrant"
+    echo "Use vagrant user"
+fi
+
+DIR=$(cd $(dirname $0); pwd)
+
+#ansible-galaxy install gdamjan.uwsgi -p ${DIR}
+ansible-galaxy install jdauphant.nginx -p ${DIR}
+ansible-playbook -i hosts -u ${USERNAME} site.yml -vvvv
